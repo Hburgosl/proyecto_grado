8839db7030d2e96e26b1aac2b0c38a979e40838a
hburgosl <hburgosl@estudiante.uniajc.edu.co>
2023-02-26 19:04:23 -0500

    Creación componente articulo

--- Frontend/proyecto_grado/src/app/app.module.ts
+++ Frontend/proyecto_grado/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './articulo/listar/listar.component';
+import { AgregarComponent } from './articulo/agregar/agregar.component';
+import { ModificarComponent } from './articulo/modificar/modificar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AgregarComponent,
+    ModificarComponent
   ],
   imports: [
     BrowserModule,
