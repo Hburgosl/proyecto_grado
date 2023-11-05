6ffece18678bc1f58c7deba448be315acb9a5501
hburgosl <hburgosl@estudiante.uniajc.edu.co>
2023-11-05 17:42:55 -0500

    Se crea metodo enviar mensaje

--- Frontend/proyecto_grado/src/app/chat/chat.component.ts
+++ Frontend/proyecto_grado/src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from '../mensaje/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -10,6 +11,22 @@ import * as SockJS from 'sockjs-client';
 export class ChatComponent {
   private client: Client;
   conectado: boolean = false;
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
+
+  constructor() {
+    this.mensaje = {
+      id_chat: {
+        id_chat: 1,
+      },
+      documento_usuario: {
+        documento_usuario: 1234197828,
+      },
+      id_existe: {
+        id_existe: 4,
+      },
+    } as any;
+  }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -22,6 +39,12 @@ export class ChatComponent {
     this.client.onConnect = (frame) => {
       console.log('Conectado ' + this.client.connected + '+' + frame);
       this.conectado = true;
+
+      this.client.subscribe('/chat/mensaje', (e) => {
+        let mensaje = JSON.parse(e.body);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
     };
 
     this.client.onDisconnect = (frame) => {
@@ -36,5 +59,13 @@ export class ChatComponent {
 
   desconectar(): void {
     this.client.deactivate();
+  }
+
+  enviarMensaje(): void {
+    this.client.publish({
+      destination: '/app/chat',
+      body: JSON.stringify(this.mensaje),
+    });
+    this.mensaje.texto = '';
   }
 }
--- Frontend/proyecto_grado/src/app/mensaje/mensaje.ts
+++ Frontend/proyecto_grado/src/app/mensaje/mensaje.ts
@@ -4,7 +4,7 @@ import { Usuario } from '../usuario/usuario';
 
 export class Mensaje {
   id_mensaje: number;
-  texto: String;
+  texto: String = '';
   fecha_envio: Date;
   id_chat: Chat;
   documento_usuario: Usuario;
