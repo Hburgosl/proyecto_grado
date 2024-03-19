70f01ddc8dd032f857361ba30c30ffc72def591b
hburgosl <hburgosl@estudiante.uniajc.edu.co>
2024-03-19 11:48:04 -0500

    Ultimos cambios del proyecto

--- Frontend/proyecto_grado/src/app/footer/footer.component.ts
+++ Frontend/proyecto_grado/src/app/footer/footer.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
-  public autor: any = {nombre: 'Howen', apellido: 'Burgos Leguizamon'}
+  public autor: any = {
+    nombre: 'Institución universitaria Antonio José Camacho',
+    apellido: 'Burgos Leguizamon',
+  };
 }
